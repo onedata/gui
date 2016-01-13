a667633684894a33192318a18ca416f280cfd7f6
Łukasz Opioła <lukasz.opiola@gmail.com>
2016-01-13 13:47:11 +0100

    VFS-1459 improve some comments

--- include/gui.hrl
+++ include/gui.hrl
@@ -1,19 +1,23 @@
 -author("lopiola").
 
+%% Types of session requirements
 -define(SESSION_ANY, any).
 -define(SESSION_LOGGED_IN, logged_in).
 -define(SESSION_NOT_LOGGED_IN, not_logged_in).
 
-% Prefix of paths requested by webscoket clients.
+%% Prefix of paths requested by webscoket clients.
 -define(WEBSOCKET_PREFIX_PATH, "/ws/").
 
-
-
-
-% GUI routing plugin
+%% GUI routing plugin - module of such name is expected in
+%% the application including gui.
+%% @todo this could be configurable from env - do we need so?
 -define(GUI_ROUTE_PLUGIN, gui_route_plugin).
+%% GUI session plugin - module of such name is expected in
+%% the application including gui.
+%% @todo this could be configurable from env - do we need so?
 -define(GUI_SESSION_PLUGIN, gui_session_plugin).
 
+%% Record used to define GUI routes, their requirements and logic.
 -record(gui_route, {
     % Does this resource require being logged in?
     requires_session = ?SESSION_ANY ::
