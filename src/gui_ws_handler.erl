5e597d9c10d3a41894cc1e80ceb406e7098b7aff
Łukasz Opioła <lukasz.opiola@gmail.com>
2016-04-11 15:42:38 +0200

    VFS-1770 remove unused define

--- src/gui_ws_handler.erl
+++ src/gui_ws_handler.erl
@@ -58,7 +58,6 @@
 -define(KEY_RESOURCE_IDS, <<"resourceIds">>).
 -define(KEY_DATA, <<"data">>).
 -define(KEY_RESULT, <<"result">>).
--define(KEY_FILTER, <<"filter">>).
 %% Message types, identified by ?KEY_MSG_TYPE key
 -define(TYPE_MODEL_REQ, <<"modelReq">>).
 -define(TYPE_MODEL_RESP, <<"modelResp">>).
