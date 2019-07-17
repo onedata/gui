70a675972b11302df130aaa4f4e76e01fc8a42f2
Wojciech Geisler <wojciech.geisler@gmail.com>
2019-07-17 14:59:18 +0200

    VFS-5629 Use correct typespec in gui record

--- include/gui.hrl
+++ include/gui.hrl
@@ -13,17 +13,17 @@
 % Record holding config of gui listener
 -record(gui_config, {
     port = 443 :: integer(),
-    key_file :: filename:filename(),
-    cert_file :: filename:filename(),
+    key_file :: file:filename(),
+    cert_file :: file:filename(),
     % Will be omitted if the path does not exist
-    chain_file = undefined :: undefined | filename:filename(),
+    chain_file = undefined :: undefined | file:filename(),
     number_of_acceptors = 100 :: integer(),
     max_keepalive = 30 :: integer(),
     request_timeout = timer:seconds(10) :: integer(),
     inactivity_timeout = timer:minutes(5) :: integer(),
     dynamic_pages = [] :: [{Path :: string(), AllowedMethods :: [gui:method()], module()}],
     custom_cowboy_routes = [] :: [{Path :: string() | binary(), module(), State :: term()}],
-    static_root = undefined :: undefined | filename:filename(),
+    static_root = undefined :: undefined | file:filename(),
     custom_response_headers = undefined :: undefined | fun(() -> cowboy:http_headers())
 }).
 
